ddf012480e23da23b8de893956762a85da50e6d3
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-09-28 22:46:48 -0700

    updated

--- posts/views.py
+++ posts/views.py
@@ -7,4 +7,4 @@ from .models import Post
 class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
-    context_object_name = 'all_posts_list'  # customizes object for for loops
+    context_object_name = 'all_posts_list'  # customizes object for for loop
